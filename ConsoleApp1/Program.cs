4b15a3c760100fb0d4dd1510f90e7b8ea14b6177
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-12-08 18:22:04 +0500

    Task4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Math.Sin(-5) + 2.0 * -5 / 3.0 - Math.Cos(-5) * 4.0 * -5);
--- Tyuiu.MarakovAD.Sprint6.Task4.V15.Lib/DataService.cs
+++ Tyuiu.MarakovAD.Sprint6.Task4.V15.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.MarakovAD.Sprint6.Task4.V15.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                res = Math.Round(Math.Sin(x) + (2*x/3) - Math.Cos(x) * 4 * x, 2);
+                res = Math.Round(Math.Sin(x) + 2.0 * x / 3.0 - Math.Cos(x) * 4.0 * x, 2);
                 valueArray[count] = res;
                 count++;
             }

06b8f90fb3e1a8eab7b4c7c0d3a9ebda0cf1fe75
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-12-09 20:20:30 +0500

    Task7

--- Tyuiu.MarakovAD.Sprint6.Task7.V4.Lib/DataService.cs
+++ Tyuiu.MarakovAD.Sprint6.Task7.V4.Lib/DataService.cs
@@ -22,9 +22,9 @@ namespace Tyuiu.MarakovAD.Sprint6.Task7.V4.Lib
             {
                 for (int j = i + 1; j < columns; j++)
                 {
-                    if (arrayalues[2, j] % 4 == 0)
+                    if (arrayalues[i, 2] % 4 == 0)
                     {
-                        arrayalues[2, j] = 4;
+                        arrayalues[i, 2] = 4;
                     }
                 }
             }
